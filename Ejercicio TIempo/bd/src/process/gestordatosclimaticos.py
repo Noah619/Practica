0eb1a2d4c7143a13638eca0e9b3a8064f04b00e5
Noah619 <166594498+Noah619@users.noreply.github.com>
2025-01-28 16:38:02 +0100

    Update gestordatosclimaticos.py
    process de bd acabado

--- Ejercicio TIempo/bd/src/process/gestordatosclimaticos.py
+++ Ejercicio TIempo/bd/src/process/gestordatosclimaticos.py
@@ -1,25 +1,39 @@
+from pymongo import MongoClient
+
 # Clase GestorDeDatosClimaticos para manejar el flujo de datos y presentación
+
+
 import json
-from basededatos import almacenaiento_bd
 from beans.localizador import Localizador
+from basededatos import base_de_datos
 
-
+# hay que crear una clase, en este caso gestor de datos climaticos, para gestionar los datos climaticos
 class GestorDeDatosClimaticos:
-    ubicaciones = []
+
+
     def __init__(self):
+        self.bd = base_de_datos.basedatos() #con esto estamos creando una variable que se llama bd inicializada con la clase de basededatos
+        # self.bd.vaciarcoleccion()
         print("Iniciando gestor de datos climaticos")
         print(f"Numero de ubicaciones actuales: {self.get_numero_ubicaciones()}")
 
-    def get_numero_ubicaciones(self):
-        return len(self.ubicaciones)
 
+    def get_numero_ubicaciones(self):
+        loc = self.bd.numerolocalizaciones()
+        return(loc)
+
+    # con esta funcion lo que hacemos es mostrar los cp y provincias que hay en la bd
     def mostrar_codigos_postales_y_provincias_almacenadas(self):
+        listalocalizaciones = self.bd.obtenerlocalizaciones()
         provincias_codigos_postales = {}
-        for ubicacion in self.ubicaciones:
-            if ubicacion.provincia in provincias_codigos_postales:
-                provincias_codigos_postales[ubicacion.provincia].append(ubicacion.codigo_postal)
+        for ubicacion in listalocalizaciones:
+            provincia = ubicacion["provincia"]
+            codigopostal = ubicacion["codigo_postal"]
+            if provincia in provincias_codigos_postales: # si la provincia está en la bd, añadimos el cp a la lista
+                provincias_codigos_postales[provincia].append(codigopostal)
+            # sino, creamos una nueva entrada con la provincia y el cp
             else:
-                provincias_codigos_postales[ubicacion.provincia] = [ubicacion.codigo_postal]
+                provincias_codigos_postales[provincia] = [codigopostal]
 
         if provincias_codigos_postales:
             print(json.dumps(provincias_codigos_postales, indent=2, ensure_ascii=False))
@@ -28,35 +42,30 @@ class GestorDeDatosClimaticos:
 
     # metodos necesarios
     def insertar_nueva_ubicacion(self,latitud, longitud):
-        ubicacion_encontrada = False
-        for ubicación in self.ubicaciones:
-            if ubicación.check_lat_lng(latitud, longitud):
-                ubicacion_encontrada = True
+        self.bd.buscarporlatlong(latitud,longitud)
+        ubicacion_encontrada = self.bd.buscarporlatlong(latitud,longitud)
+        if ubicacion_encontrada!= None:
+
                 print("================================================")
                 print("Ubicación ya existe")
-                print(ubicación.mostrar_informacion())
+                print("latitud: ", ubicacion_encontrada.latitud," longitud: ",ubicacion_encontrada.longitud," ciudad: ",ubicacion_encontrada.ciudad)
                 print("================================================")
-                break
-        
-        if not ubicacion_encontrada:
-            p = (latitud, longitud)
-            almacenaiento_bd.insertar_localización(p.to_dict())
+        else: 
+            nuevalocalizacion=Localizador(latitud,longitud)#con esto obtenemos ciudad, cp, barrio; a traves de la clase localizador dandoles solo la long y lat
+            
+            tabla=nuevalocalizacion.to_dict()# y con esto, convertimos a tabla hash
+            self.bd.insertarubicacion(tabla)
+            #almacenar en la base de datos la tabla hash de la clase nuevalocalizacion
             print("Ubicación agregada correctamente")
-        else:
-            print("Ubicación ya existe")
+ 
+            
         return ubicacion_encontrada
 
     def buscar_por_codigo_postal(self,codigo_postal):
-        ubicacion_encontrada = None
-        for ubicacion in self.ubicaciones:
-            if ubicacion.codigo_postal == codigo_postal:
-                ubicacion_encontrada = ubicacion
-                break
-        return ubicacion_encontrada
+        lista_ubicaciones = self.bd.buscarporcp(codigo_postal)
+        return lista_ubicaciones
     
     def buscar_por_provincia(self,provincia):
-        lista_ubicaciones = []
-        for ubicacion in self.ubicaciones:
-            if ubicacion.provincia == provincia:
-                lista_ubicaciones.append(ubicacion)
+        lista_ubicaciones = self.bd.buscarporprov(provincia)
+
         return lista_ubicaciones
